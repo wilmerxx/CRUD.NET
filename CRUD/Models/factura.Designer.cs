c3990ac67daf4138400606c43ed20881eb39774a
wilmerxx <wmshagnay@espe.edu.ec>
2023-01-07 19:56:33 -0500

    cambio de version

--- CRUD/Models/factura.Designer.cs
+++ CRUD/Models/factura.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\admin\source\repos\CRUD\CRUD\Models\factura.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'D:\Archivos de Wilmer\Septimo semestre\Segundo Parcial\Desarrollo web\LABORATORIO 2\CRUD\CRUD.NET\CRUD\Models\factura.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
