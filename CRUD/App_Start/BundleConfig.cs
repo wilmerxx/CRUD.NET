f3de8d151935888a531f415957e31db36b1886d7
wilmerxx <wmshagnay@espe.edu.ec>
2023-01-07 20:39:20 -0500

    cambio de fallos en el sistema

--- CRUD/App_Start/BundleConfig.cs
+++ CRUD/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace CRUD
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
